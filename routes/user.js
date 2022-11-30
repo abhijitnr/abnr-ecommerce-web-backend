8846157d0c68e5919dc78f98c4d01c0bd79578cf
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-01 02:49:52 +0530

    userRoute is added

--- routes/user.js
+++ routes/user.js
@@ -52,9 +52,41 @@ userRoute.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
 
 /* GET ALL USER */
 userRoute.get("/", verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
   try {
-    const users = await User.find();
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/* GET USER STATS */
+userRoute.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      {
+        $match: { createdAt: { $gte: lastYear } },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
