7d93c285b90b0a6ec15fc812918c132830c322e2
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-02 18:27:47 +0530

    Order route is added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user.js");
 const authRoute = require("./routes/auth.js");
 const productRoute = require("./routes/product.js");
+const cartRoute = require("./routes/cart.js");
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,74 @@
+const {
+  verifyToken,
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+} = require("../middlewares/verifyToken");
+const Order = require("../models/Order");
+
 const orderRoute = require("express").Router();
+
+/* CREATE */
+orderRoute.post("/", verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+/* UPDATE */
+orderRoute.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(updatedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+/* DELETE */
+orderRoute.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json("Order has been deleted...");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+/* GET USER ORDERS */
+orderRoute.get(
+  "/find/:userId",
+  verifyTokenAndAuthorization,
+  async (req, res) => {
+    try {
+      const orders = await Order.find({ userId: req.params.userId });
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
+
+/* GET ALL ORDERS */
+orderRoute.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = orderRoute;
